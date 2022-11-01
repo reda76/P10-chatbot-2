0059dad4be3ae8006bb582bbcf253974b41913db
redad76 <derraz.mohamedreda@gmail.com>
2022-11-01 18:44:40 +0100

    Test debug aiohhtp

--- app.py
+++ app.py
@@ -89,6 +89,6 @@ def init_func(argv):
 if __name__ == "__main__":
     APP = init_func(None)
     try:
-        web.run_app(APP, host="localhost", port=CONFIG.PORT)
+        web.run_app(APP, host='0.0.0.0', port=CONFIG.PORT)
     except Exception as error:
         raise error
